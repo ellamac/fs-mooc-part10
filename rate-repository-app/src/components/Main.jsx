45ac61181a50cf73b663ecf636d9108bb137d3fe
Ella Mäkelä <ellamakela@Ella-MacBook-Pro.local>
2022-05-28 20:11:18 +0300

    ex 10.5

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
         flexShrink: 1,
+        backgroundColor: '#e1e4e8',
     },
 });
 
@@ -16,7 +17,7 @@ const Main = () => {
     return (
         <>
         <AppBar />
-        <View><RepositoryList /></View>
+        <View style={styles.container}><RepositoryList /></View>
         </>
     );
 };
