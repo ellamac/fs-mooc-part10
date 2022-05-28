4b3b25c400207ab2269b52e42808bed102cb6e6a
Ella Mäkelä <ellamakela@Ella-MacBook-Pro.local>
2022-05-28 22:27:52 +0300

    ex 10.7.

--- rate-repository-app/src/components/AppBarTab.jsx
+++ rate-repository-app/src/components/AppBarTab.jsx
@@ -1,11 +1,18 @@
 import Text from './Text';
+import { StyleSheet} from 'react-native';
+
 import { Link } from 'react-router-native';
 
+const styles = StyleSheet.create({
+  tab: {
+    paddingHorizontal: 10,
+  }
 
+});
 
 const AppBarTab = ({ path, ...props }) => {
   return (
-    <Link to={path} >
+    <Link to={path} style={styles.tab}>
       <Text fontWeight="bold" color="light" {...props} />
     </Link>
   );
